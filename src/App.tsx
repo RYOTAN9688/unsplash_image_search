08c9870b7d5306fed53f5f9e3e9bce5a61938f19
NAKAO RYOTA <premium.developer1122.gmail.com>
2023-10-06 17:07:34 +0900

    unsplish_image_search fixed

---
+++ src/App.tsx
@@ -0,0 +1,102 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+import "./App.css";
+import { Button, Form } from "react-bootstrap";
+import axios from "axios";
+
+const API_URL = "https://api.unsplash.com/search/photos";
+const IMAGES_PER_PAGE = 20;
+
+export const App = () => {
+  const searchInput = useRef<HTMLInputElement>(null);
+  const [page, setPage] = useState(1);
+  const [images, setImages] = useState<string[]>([]);
+  const [totalPages, setTotalPages] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  const fetchImages = useCallback(async () => {
+    try {
+      if (searchInput?.current?.value) {
+        setLoading(true);
+        const { data } = await axios.get(
+          `${API_URL}?query=${
+            searchInput?.current?.value
+          }&page=${page}&per_page=${IMAGES_PER_PAGE}&client_id=${
+            import.meta.env.VITE_API_KEY
+          }`
+        );
+        setImages(data.results);
+        setTotalPages(data.total_pages);
+        setLoading(false);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }, [page]);
+
+  useEffect(() => {
+    fetchImages();
+  }, [fetchImages]);
+
+  const resetSearch = () => {
+    setPage(1);
+    fetchImages();
+  };
+
+  const handleSearch = (e: React.FormEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    resetSearch();
+  };
+
+  const handleSelection = (selection: string) => {
+    searchInput.current.value = selection;
+    resetSearch();
+  };
+
+  return (
+    <div className="container">
+      <h1 className="title">Image Search</h1>
+      <Form onSubmit={(e) => handleSearch(e)}>
+        <Form.Control
+          type="search"
+          placeholder="Type something to search"
+          className="search-input"
+          ref={searchInput}
+        />
+      </Form>
+      <div className="search-section">
+        <div className="filters">
+          <div onClick={() => handleSelection("nature")}>Nature</div>
+          <div onClick={() => handleSelection("birds")}>Birds</div>
+          <div onClick={() => handleSelection("cats")}>Cats</div>
+          <div onClick={() => handleSelection("shoes")}>Shoes</div>
+        </div>
+      </div>
+      {loading ? (
+        <p className="loading">Loading...</p>
+      ) : (
+        <>
+          <div className="images">
+            {images.map((image) => {
+              return (
+                <img
+                  key={image.id}
+                  src={image.urls.small}
+                  alt={image.alt_description}
+                  className="image"
+                />
+              );
+            })}
+          </div>
+          <div className="buttons">
+            {page > 1 && (
+              <Button onClick={() => setPage(page - 1)}>Previous</Button>
+            )}
+            {page < totalPages && (
+              <Button onClick={() => setPage(page + 1)}>Next</Button>
+            )}
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
